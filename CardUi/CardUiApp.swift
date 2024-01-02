7ff23003e4f4b3cef7467f26f6330a9cfe2998eb
Shreyash <shreyashp47@gmail.com>
2024-01-02 07:43:14 +0530

    Initial Commit

---
+++ CardUi/CardUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardUiApp.swift
+//  CardUi
+//
+//  Created by Shreyash Pattewar on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CardUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
